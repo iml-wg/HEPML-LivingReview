b9824d31a6fc5589323cff71a5d08d41107fd24c
Johnny Raine <johnny.raine@cern.ch>
2023-05-25 14:26:20 +0200

    Adding resources and structure for mkdocs To do: CI integration

---
+++ docs/resources.md
@@ -0,0 +1,6 @@
+---
+hide:
+  - navigation
+---
+
+All papers in the review are collected into a single `.bib` file for convenience which can be downloaded here.
--- make_md.py
+++ make_md.py
@@ -4,18 +4,21 @@ import requests
 
 update_journal = False
 
-myfile = open("HEPML.tex", encoding="utf8")
-myfile_out = open("README.md","w", encoding="utf8")
+myfile_out = open("docs/about.md", encoding="utf8")
 
-myfile_out.write("#  **A Living Review of Machine Learning for Particle Physics**\n\n")
+myfile_out.write("---\nhide:\n\n  - navigation\n---\n\nThe purpose of this note is to collect references for modern machine learning as applied to particle physics.  A minimal number of categories is chosen in order to be as useful as possible.  Note that papers may be referenced in more than one category.  The fact that a paper is listed in this document does not endorse or validate its content - that is for the community (and for peer-review) to decide.  Furthermore, the classification here is a best attempt and may have flaws - please let us know if (a) we have missed a paper you think should be included, (b) a paper has been misclassified, or (c) a citation for a paper is not correct or if the journal information is now available.  In order to be as useful as possible, this document will continue to evolve so please check back before you write your next paper.  If you find this review helpful, please consider citing it using \\cite{hepmllivingreview} in HEPML.bib.\n\n")
+
+myfile_out.write("This review was built with the help of the HEP-ML community, the [INSPIRE REST API](https://github.com/inspirehep/rest-api-doc), and the moderators Benjamin Nachman, Matthew Feickert, Claudius Krause, and Ramon Winterhalder.\n\n")
+
+myfile = open("HEPML.tex", encoding="utf8")
+myfile_out = open("docs/index.md","w", encoding="utf8")
+
+myfile_out.write("---\nhide:\n\n  - navigation\n---\n\n#  **A Living Review of Machine Learning for Particle Physics**\n\n")
 
 myfile_out.write("*Modern machine learning techniques, including deep learning, is rapidly being applied, adapted, and developed for high energy physics.  The goal of this document is to provide a nearly comprehensive list of citations for those developing and applying these approaches to experimental, phenomenological, or theoretical analyses.  As a living document, it will be updated as often as possible to incorporate the latest developments.  A list of proper (unchanging) reviews can be found within.  Papers are grouped into a small set of topics to be as useful as possible.  Suggestions are most welcome.*\n\n")
 
 myfile_out.write("[![download](https://img.shields.io/badge/download-review-blue.svg)](https://iml-wg.github.io/HEPML-LivingReview/review/hepml-review.pdf)\n[![github](https://badges.aleen42.com/src/github.svg)](https://github.com/iml-wg/HEPML-LivingReview)\n\n")
 
-myfile_out.write("The purpose of this note is to collect references for modern machine learning as applied to particle physics.  A minimal number of categories is chosen in order to be as useful as possible.  Note that papers may be referenced in more than one category.  The fact that a paper is listed in this document does not endorse or validate its content - that is for the community (and for peer-review) to decide.  Furthermore, the classification here is a best attempt and may have flaws - please let us know if (a) we have missed a paper you think should be included, (b) a paper has been misclassified, or (c) a citation for a paper is not correct or if the journal information is now available.  In order to be as useful as possible, this document will continue to evolve so please check back before you write your next paper.  If you find this review helpful, please consider citing it using \\cite{hepmllivingreview} in HEPML.bib.\n\n")
-
-myfile_out.write("This review was built with the help of the HEP-ML community, the [INSPIRE REST API](https://github.com/inspirehep/rest-api-doc), and the moderators Benjamin Nachman, Matthew Feickert, Claudius Krause, and Ramon Winterhalder.\n\n")
 
 ###This bit is slightly modified from Kyle Cranmer https://github.com/cranmer/inspire_play
 def summarize_record(recid):
